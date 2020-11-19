9eb7d82c64f28f4b5ef142fd9621e011065b2f10
John Dimeo <dimeo@elderresearch.com>
2020-11-19 00:41:20 -0500

    Provide ability to debug log the R expressions. Use direct assign for single strings (which is apparently a different code path)

--- rserve/src/main/java/com/elderresearch/commons/rserve/RConnectionWrapper.java
+++ rserve/src/main/java/com/elderresearch/commons/rserve/RConnectionWrapper.java
@@ -10,7 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.val;
 import lombok.experimental.Accessors;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Accessors(fluent = true)
 public class RConnectionWrapper {
@@ -20,7 +22,9 @@ public class RConnectionWrapper {
 	// Rserve doesn't have a REPL so error messages are lost. Capture them in a try and throw an exception
 	// See: http://rforge.net/Rserve/faq.html#errors
 	public REXP tryEval(String s, Object... args) throws REngineException, REXPMismatchException {
-		val ret = delegate.parseAndEval("try(" + String.format(s, args) + ", silent=TRUE)");
+		val exp = "try(" + String.format(s, args) + ", silent=TRUE)";
+		log.debug("R: {}", exp);
+		val ret = delegate.parseAndEval(exp);
 		if (ret.inherits("try-error")) { throw new REngineException(delegate, ret.asString()); }
 		return ret;
 	}
@@ -28,5 +32,11 @@ public class RConnectionWrapper {
 	public void assign(String s, byte...   arr) throws REngineException { delegate.assign(s, arr); }
 	public void assign(String s, int...    arr) throws REngineException { delegate.assign(s, arr); }
 	public void assign(String s, double... arr) throws REngineException { delegate.assign(s, arr); }
-	public void assign(String s, String... arr) throws REngineException { delegate.assign(s, arr); }
+	public void assign(String s, String... arr) throws REngineException {
+		if (arr.length == 1) {
+			delegate.assign(s, arr[0]);
+		} else {
+			delegate.assign(s, arr);
+		}
+	}
 }
